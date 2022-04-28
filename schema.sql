d6c5f0424d3d0c7c59fc63ea05d2b58fff91ff5d
Omar Ragheb <ahmed.raph29@gmail.com>
2022-04-28 11:30:07 +0200

    Adding new tables and foreign keys

--- schema.sql
+++ schema.sql
@@ -22,3 +22,41 @@ CREATE TABLE animals (
 -- Add a column species of type string to your animals table
 ALTER TABLE animals
 		ADD species TEXT;
+
+-- Create a table named owners:
+    CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name TEXT,
+	age INT,
+    PRIMARY KEY(id)
+);
+
+-- Create a table named species
+    CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name TEXT,
+	PRIMARY KEY(id)
+);
+
+
+-- Modify animals table
+    BEGIN;
+
+    ALTER TABLE animals
+    DROP COLUMN species;
+
+    ALTER TABLE animals ADD COLUMN species_id INT;
+
+    ALTER TABLE animals
+    ADD CONSTRAINT fk_species
+    FOREIGN KEY (species_id)
+    REFERENCES species (id);
+
+    ALTER TABLE animals ADD COLUMN owner_id INT;
+
+    ALTER TABLE animals
+    ADD CONSTRAINT fk_owner
+    FOREIGN KEY (owner_id)
+    REFERENCES owners (id);
+
+    COMMIT;
